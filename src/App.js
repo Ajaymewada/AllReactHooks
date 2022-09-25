b861f0ed85f793ae4000715ac5f9cdf1978e2bdd
ajay <ajay.mevada@coartha.com>
2022-09-25 15:10:45 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React With Charan
+          Learn React With Ajay Kumar
         </a>
       </header>
     </div>
